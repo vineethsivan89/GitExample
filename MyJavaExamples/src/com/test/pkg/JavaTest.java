8d8b40fd11723a095da97aee24b1b187b4fa35f5
ut53znk <ut53znk@VPE1-D00160.wwg00m.rootdom.net>
2018-01-03 14:21:50 +0530

    modified from eclipse

--- MyJavaExamples/src/com/test/pkg/JavaTest.java
+++ MyJavaExamples/src/com/test/pkg/JavaTest.java
@@ -3,7 +3,7 @@ package com.test.pkg;
 public class JavaTest {
 
 	public static void main(String[] args) {
-		System.out.println("hai vineeth");
+		System.out.println("hai vineeth sivan nair");
 
 	}
 
