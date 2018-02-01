88274b1c90ca4bcfbe3f0a73ff64bbb5f50486ad
ut53znk <ut53znk@VPE1-D00160.wwg00m.rootdom.net>
2018-02-01 12:38:33 +0530

    Commiting Atest

---
+++ MyJavaExamples/src/com/test/pkg/Atest.java
@@ -0,0 +1,13 @@
+package com.test.pkg;
+
+public abstract class Atest extends Sample{
+	
+	public Atest(){
+		super(10,"Sachin");
+	}
+	
+	public static void display(){
+		System.out.println(" i am going to dispaly");
+	}
+
+}
